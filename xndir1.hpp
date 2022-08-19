aff0a66b855b261e260181e0e74919a5c977c372
ArmenParanyan <80101033+ArmParanyan@users.noreply.github.com>
2022-08-19 16:42:21 +0400

    Update xndir1.hpp

--- xndir1.hpp
+++ xndir1.hpp
@@ -1,11 +1,36 @@
-// #ifndef XNDIR_1_HPP
-// #define XNDIR_1_HPP
-
 #include "xndir1.h"
 
 Mlass::Mlass(std::string name, std::string surname, int age) {
+    if (name[0] >= 'A' && name[0] <= 'Z') {
+        for (int i = 1; i < name.size(); ++i) {
+            if (name[i] < 'a' || name[i] > 'z') {
+                std::cout << "invalid name type:";
+                return;
+            } 
+        }
+    } else  if (name[0] < 'A' || name[0] > 'Z') {
+        std::cout << "invalid name type:";
+        return;
+    }
     this->name = name;
+
+    if (surname[0] >= 'A' && surname[0] <= 'Z') {
+        for (int i = 1; i < surname.size(); ++i) {
+            if (surname[i] < 'a' || surname[i] > 'z') {
+                std::cout << "invalid name type:";
+                return;
+            } 
+        }
+    } else  if (surname[0] < 'A' || surname[0] > 'Z') {
+        std::cout << "invalid name type:";
+        return;
+    }
     this->surname = surname;
+
+    if (age < 0 || age > 100) { 
+        std::cout << "invalid age: ";
+        return;
+    }
     this->age = age;
 }
 
@@ -31,8 +56,13 @@ std::string Mlass::get_value() {
     return ret;
 }
 
-Mlass& Mlass::operator=(const Mlass&& oth) {
-    if (this != )
+Mlass& Mlass::operator=(Mlass&& obj) {
+if (this != &obj)
+    {
+        this->name = obj.name;
+        this->age = obj.age;
+        obj.name = {};
+        obj.age = {};
+    }
+    return *this;
 }
-
-// #endif //XNDIR_1_HPP
